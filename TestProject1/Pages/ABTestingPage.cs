1166a0aa729b45e0235030dbebb4b39825047aad
pawelf444@wp.pl <pawelf444@wp.pl>
2021-06-18 20:12:36 +0200

    Generic type for NavigateTo sub pages

--- TestProject1/Pages/ABTestingPage.cs
+++ TestProject1/Pages/ABTestingPage.cs
@@ -6,7 +6,9 @@ namespace TestProject1.Pages
     {
         public const string PageTitle = "The Internet";
         public ABTestingPage(IWebDriver driver) : base(driver) { }
-            
+
+        public ABTestingPage() { }
+
         public IWebElement MainHeader => Driver.FindElement(By.XPath("/html/body/div[2]/div/div/h3"));
         public IWebElement MainFooter => Driver.FindElement(By.XPath("/html/body/div[3]/div/div/a"));
         public IWebElement Content => Driver.FindElement(By.XPath("/html/body/div[2]/div/div/p"));
